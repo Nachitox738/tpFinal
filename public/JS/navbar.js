0f3fa0735fbe2fffd501211e868c15563540334b
Nachitox738 <ares.ignacio@tecnica7.edu.ar>
2024-11-27 00:09:09 -0300

    sddddddkd

--- public/JS/navbar.js
+++ public/JS/navbar.js
@@ -1,5 +1,5 @@
 const navbar = `
-<nav class="navbar navbar-expand-lg bg-warning">
+<nav class="navbar navbar-expand-lg bg-danger">
     <div class="container-fluid">
         <a class="navbar-brand text-dark" href="index.html">Guantes y Co.</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
